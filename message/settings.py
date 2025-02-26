24b78158997aa7c9c0bcdba311fb8d82f1d7772b
Parlee Khadka <khadkaparlee@gmail.com>
2025-02-25 23:09:30 -0800

    first 1

--- message/settings.py
+++ message/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6^vhs8jj+8x--+i04ix6@m=f$y^u-&!eat!ctuqdl-6g&hc#p#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
